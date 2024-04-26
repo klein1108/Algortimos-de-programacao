2d22491c5442568f324be8cf3980bfd3499567db
Lucas Klein <lk.lucasklein@gmail.com>
2024-04-25 22:21:31 -0300

    adicionando questao 4 da lista de strings

--- strings/lista1/Atividade04.c
+++ strings/lista1/Atividade04.c
@@ -13,44 +13,57 @@
 #define FALSE 0
 
 int main(){
-    int contaLetra = 0, contaPalavra = 0, isPalavra = FALSE;
+    int contaLetra = 0, contaPalavra = 0, tamanhoTexto = 0;
+    int isPalavra = FALSE;
     char texto[MAX_CARACTERES * MAX_PALAVRAS + 1] = "";
-    char palavrasDoTexto[MAX_CARACTERES][MAX_PALAVRAS] = {};
+    char palavrasDoTexto[MAX_PALAVRAS][MAX_CARACTERES] = {{}};
+    char menorPalavra[MAX_CARACTERES];
 
     printf("Digite um texto de no maximo %d palavras, com no maximo %d caracteres: \n", MAX_PALAVRAS, MAX_CARACTERES);
     fgets(texto, MAX_CARACTERES * MAX_PALAVRAS + 1, stdin);
 
+    tamanhoTexto= strlen(texto) - 1;
 
-    for(int i = 0; texto[i] != FINAL_TEXTO; i++){ // {b,a,n,a,n,a, ,s,l,a,\0}
-        //CASO ESPERTINHO: comecar com espaco, virgula e afins.
-
-        //achar o final da palavra
+    for(int i = 0; i < tamanhoTexto; i++){
         if(texto[i] == ' ' || texto[i] == '.' || texto[i] == ','){
             if(isPalavra){
                 contaLetra = 0;
                 contaPalavra++;
-                printf("\n");
+                isPalavra = FALSE;
             }
         } else {
-            palavrasDoTexto[contaLetra][contaPalavra] = texto[i];
+            palavrasDoTexto[contaPalavra][contaLetra] = texto[i];
             contaLetra++;
-            printf("%c ", texto[i]);
+            isPalavra = TRUE;
         }
     }
 
-    /**
-    {
-        {'a', 'b', 'c'},
-        {'a', 'b', 'c'},
-        {'a', 'b', 'c'},
-        {'a', 'b', 'c'}
+
+//  ordenar palavras em ordem alfabetica
+    for(int i = 0; i <= contaPalavra; i++){
+        for(int j = i; j<=contaPalavra; j++){
+            int comparacao = strcmp(palavrasDoTexto[i], palavrasDoTexto[j]);
+            if(comparacao > 0 ){
+                strcpy(menorPalavra, palavrasDoTexto[j]);
+                strcpy(palavrasDoTexto[j], palavrasDoTexto[i]);
+                strcpy(palavrasDoTexto[i], menorPalavra);
+            }
+        }
+
     }
 
-    */
+    for(int i = 0; i<= contaPalavra; i++){
+        printf("%d: %s  -> length: %d\n",i, palavrasDoTexto[i], strlen(palavrasDoTexto[i]));
+        /**
+        {
+            {'a', 'b', 'c'}, == string
+            {'g', 'h', 'i'}, == string
+            {'d', 'e', 'f'}, == string
+            {'j', 'k', 'l'}, == string
+            ...
+        }
+        */
+    }
     return 0;
 }
-/**
-Um texto � composto de palavras de 10 ou menos caracteres. Escreva um
-programa que l� um texto de at� 100 palavras e depois as imprime em ordem
-alfab�tica.
-*/
+
